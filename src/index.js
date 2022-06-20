28a3cfe9e127790a327d00840552e05cb53704cd
Teslav-code <teslav93@mail.ru>
2022-06-20 14:27:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import Counter from './components/counter';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './reduser';
+import App from './components/app';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const store = createStore(reducer);
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  </React.StrictMode>,
+document.getElementById('root'));
